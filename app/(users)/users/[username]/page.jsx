27aa47962803edaf1f77a9966139c74beebe9be9
thapa technical <vbthapa55@gmail.com>
2025-06-20 12:30:37 +0530

    dynamic route segments

---
+++ app/(users)/users/[username]/page.jsx
@@ -0,0 +1,8 @@
+const SingleProfile = async (props) => {
+  const user = await props.params;
+  console.log(user);
+
+  return <h1>user = {user.username}</h1>;
+};
+
+export default SingleProfile;
---
+++ app/(users)/users/[username]/posts/[postId]/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { use } from "react";
+
+const SingleProfilePost = (props) => {
+  const user = use(props.params);
+  console.log(user);
+
+  return (
+    <h1>
+      user : {user.username}, PostId : {user.postId}
+    </h1>
+  );
+};
+
+export default SingleProfilePost;
