5a0777a16d3aec6141527e7a2c2763ccf6f4e6aa
thapa technical <vbthapa55@gmail.com>
2025-06-28 19:38:34 +0530

    nextjs joke app

--- app/(users)/datafetch/servercomp/page.jsx
+++ app/(users)/datafetch/servercomp/page.jsx
@@ -4,6 +4,12 @@ const DataFetchServer = async (props) => {
   const searchParams = await props.searchParams;
   const userName = searchParams.name;
 
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  });
+
   // Handle case where no name is provided
   if (!userName) {
     return (
--- app/projects/jokes/page.jsx
+++ app/projects/jokes/page.jsx
@@ -1,19 +1,21 @@
 "use client";
-import { use, useEffect, useState } from "react";
 
-export default function CoffeeJokeCard() {
-  const [showPunchline, setShowPunchline] = useState(false);
+import { useEffect, useState } from "react";
+
+const RandomJokes = () => {
+  const [randomJokes, setRandomJokes] = useState({});
+  const [showJokes, setShowJokes] = useState(true);
+
+  const URL = "https://official-joke-api.appspot.com/random_joke";
+  const fetchRandomJokes = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    console.log(data);
+    setRandomJokes(data);
+  };
 
   useEffect(() => {
-    const fetchKJokes = async () => {
-      const res = await fetch(
-        "https://official-joke-api.appspot.com/random_joke"
-      );
-      const data = await res.json();
-      //   console.log(res);
-      setShowPunchline(data.punchline);
-    };
-    fetchKJokes();
+    fetchRandomJokes();
   }, []);
 
   return (
@@ -21,28 +23,31 @@ export default function CoffeeJokeCard() {
       <div className="bg-white rounded-xl shadow-lg p-8 max-w-md w-full border-2 border-amber-200">
         <div className="text-center">
           <div className="text-6xl mb-4">☕</div>
-          <h2 className="text-2xl font-bold text-gray-800 mb-6">Coffee Joke</h2>
+          <h2 className="text-2xl font-bold text-gray-800 mb-6">
+            Random Jokes Generator
+          </h2>
 
           <div className="space-y-4">
             <p className="text-lg text-gray-700 font-medium">
-              Why did the coffee file a police report?
+              {randomJokes.setup}
+              {/* {randomJokes.} */}
             </p>
 
-            {!showPunchline ? (
+            {showJokes ? (
               <button
-                onClick={() => setShowPunchline(true)}
-                className="bg-amber-500 hover:bg-amber-600 text-white font-semibold py-3 px-6 rounded-lg transition-colors duration-200 shadow-md hover:shadow-lg"
+                onClick={() => setShowJokes(false)}
+                className="bg-amber-500 hover:bg-amber-600 text-white font-semibold py-3 px-6 rounded-lg transition-colors duration-200 shadow-md cursor-pointer hover:shadow-lg"
               >
                 Reveal Punchline
               </button>
             ) : (
               <div className="animate-fade-in">
-                <p className="text-xl text-amber-700 font-bold bg-amber-50 p-4 rounded-lg border-2 border-amber-200">
-                  It got mugged! 😄
+                <p className="text-xl text-amber-700 font-bold bg-amber-50 p-4 rounded-lg border-2 border-amber-200 ">
+                  {randomJokes.punchline} 😄
                 </p>
                 <button
-                  onClick={() => setShowPunchline(false)}
-                  className="mt-4 bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
+                  onClick={() => setShowJokes(true)}
+                  className="mt-4 bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200 cursor-pointer"
                 >
                   Hide Punchline
                 </button>
@@ -50,9 +55,19 @@ export default function CoffeeJokeCard() {
             )}
           </div>
 
-          <div className="mt-6 text-sm text-gray-500">Joke ID: 431</div>
+          <div>
+            <button
+              onClick={fetchRandomJokes}
+              className="text-l text-amber-700 font-bold bg-amber-50 px-4 mt-3 rounded-lg border-2 border-amber-200 cursor-pointer hover:shadow-lg"
+            >
+              Next
+            </button>
+            <div className="mt-6 text-sm text-gray-500">Joke ID: 431</div>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default RandomJokes;
